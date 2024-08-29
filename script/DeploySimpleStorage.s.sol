bf171b91c1cd7cdae891619ae499cab410aee397
Muhangi2 <eliodamutiba@gmail.com>
2024-08-29 23:20:14 +0300

    deploycontract

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Script} from  "../src/SimpleStorage"
+import {SimpleStorage} from "../src/SimpleStorage.sol"
+
+contract DeploySimpleStorage is Script{
+      function run () external returns (SimpleStorage){
+        vm.startBroadcast();
+        SimpleStorage simpleStorage=new SimpleStorage();
+        vm.stopBroadcast();
+      }
+}
